05aa9449bfa874f4e6ef7cfe4887477757b3bf3e
varooon <varuns@Varuns-MacBook-Pro.local>
2016-11-15 20:33:28 -0800

    First commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+  //Select a random value between 1 & 3.
+  var computerChoice = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+  console.log(computerChoice);
+
+  //Set rock, paper, scissors values
+  var rock = 1;
+  var paper = 2;
+  var scissors = 3;
+
+  $("#rock").on("click", function(){
+    if (computerChoice === rock){
+      $("#rock").addClass("tie");
+      $("#display-winner").html("It's a tie!").css("color", "#E3B930");
+    } else if (computerChoice === paper) {
+      $("#rock").addClass("failure");
+      $("#display-winner").html("Computer wins!").css("color", "#E33030");
+    } else if (computerChoice === scissors) {
+        $("#rock").addClass("success");
+        $("#display-winner").html("Player wins!").css("color", "#51B551");
+    }
+  });
+
+  $("#paper").on("click", function(){
+    if (computerChoice === paper){
+      $("#paper").addClass("tie");
+      $("#display-winner").html("It's a tie!").css("color", "#E3B930");
+    } else if (computerChoice === scissors) {
+      $("#paper").addClass("failure");
+      $("#display-winner").html("Computer wins!").css("color", "#E33030");
+    } else if (computerChoice === rock) {
+      $("#paper").addClass("success");
+      $("#display-winner").html("Player wins!").css("color", "#51B551");
+    }
+  });
+
+  $("#scissors").on("click", function(){
+    if (computerChoice === scissors){
+      $("#scissors").addClass("tie");
+      $("#display-winner").html("It's a tie!").css("color", "#E3B930");
+    } else if (computerChoice === rock) {
+      $("#scissors").addClass("failure");
+      $("#display-winner").html("Computer wins!").css("color", "#E33030");
+    } else if (computerChoice === paper) {
+      $("#scissors").addClass("success");
+      $("#display-winner").html("Player wins!").css("color", "#51B551");
+    }
+  });
+
+});
